79b0102ddeea8d71cc4b897fda7dec6f679d5bf2
Arnob <arnob2601@yahoo.com>
2023-05-11 03:33:14 -0400

    GCN with linear compressors and AutoEncoder

--- modules/gcn/gcn/learning/models.py
+++ modules/gcn/gcn/learning/models.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
 
 from torch.nn.parameter import Parameter
 from torch.nn.modules.module import Module
@@ -62,3 +63,42 @@ class GCN(nn.Module):
         x = F.dropout(x, self.dropout, training=self.training)
         x = self.gc2(x, adj)
         return F.log_softmax(x, dim=1)
+
+
+# Define the GCN model
+
+class nGCN(torch.nn.Module):
+    def __init__(self, hidden_channels, args=None):
+        super(nGCN, self).__init__()
+        torch.manual_seed(8616)
+        self._args = args
+        self.fc1 = nn.Linear(2, 8)
+        self.fc2 = nn.Linear(8, 8)
+        self.fc3 = nn.Linear(8, 8)
+        self.conv1 = GCNConv(8, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, 1)
+
+        self.bn1 = nn.BatchNorm1d(8)
+        self.bn2 = nn.BatchNorm1d(8)
+        self.bn3 = nn.BatchNorm1d(8)
+
+    def forward(self, x, edge_index, device='cpu'):
+        x = x.to(device)
+        edge_index = edge_index.to(device)
+        x = F.leaky_relu(self.bn1(self.fc1(x)), 0.1)
+        x = F.leaky_relu(self.bn1(self.fc2(x)), 0.1)
+        x = F.leaky_relu(self.bn1(self.fc3(x)), 0.1)
+        x = F.leaky_relu(self.conv1(x, edge_index), 0.1)
+        x = F.dropout(x, training=self.training)
+        x = self.conv2(x, edge_index)
+        return x  # F.log_softmax(x, dim=1)
+    
+    def loss(self, output, target, writer=None, index=None):
+        loss_fn = torch.nn.MSELoss()
+        loss = torch.sqrt(loss_fn(output, target.unsqueeze(1)))
+        # Logging
+        if writer is not None:
+            writer.add_scalar("Loss",
+                              loss.item(),
+                              index)
+        return loss
